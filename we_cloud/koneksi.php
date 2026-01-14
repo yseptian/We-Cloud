6015579d50b1d826c06ad453cf3d1fe8440bf42f
yseptian <yseptian@student.ciputra.ac.id>
2026-01-14 17:59:11 +0700

    Update database connection details in koneksi.php

--- we_cloud/koneksi.php
+++ we_cloud/koneksi.php
@@ -1,15 +1,18 @@
 <?php
-$host = "localhost";   // MySQL Workbench
-$user = "root";        // username MySQL
-$pass = "samsungS21fe+"; // isi sesuai Workbench
-$db   = "db_cloud";
+$host = "sql211.infinityfree.com";   // MySQL Workbench
+$user = "if0_40901908";        // username MySQL
+$pass = "samsungS21fe"; // isi sesuai Workbench
+$db   = "if0_40901908_db_cloud";
 $port = 3306;          // cek di Workbench
 
 $koneksi = mysqli_connect($host, $user, $pass, $db, $port);
-$main_url = "http://localhost/we_cloud/";
+mysqli_query($koneksi, "SET time_zone = '+07:00'");
+
 
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
+
+
 
